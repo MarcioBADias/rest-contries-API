4d9e366fbc4d4fda4e8079346867eef9b93a806f
MarcioBADias <marcio.b.a.dias@gmail.com>
2023-01-16 22:35:55 -0300

    ajustando o consumo da API e seus objetos

--- app/app.js
+++ app/app.js
@@ -1,11 +1,14 @@
-const getAPI = async url =>{
-    const response = await fetch(url).json()
-    return await response;
+const countryAPI = 'https://restcountries.com/v3.1/all'
+
+const getAPI = async () => {
+    const response = await fetch(countryAPI)
+    const json = await response.json()
+    console.log(json[0])
+    console.log('nome: ',json[0].name.common)
+    console.log('capital: ',json[0].capital[0])
+    console.log('região: ',json[0].region)
+    console.log('bandeira: ',json[0].flags.svg)
+    console.log('população: ',json[0].population)
 }
 
-const countryDatas = async url => {
-    const datas = await getAPI(url);
-    console.log(data)
-}
-
-countryDatas('')
+getAPI()
